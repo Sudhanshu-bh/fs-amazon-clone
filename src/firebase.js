189d615d3f258b9fa251652fe94d8f8f41f1fd3d
Sudhanshu <bsudhanshu3@gmail.com>
2021-06-09 18:13:10 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDfOL_MkdYr-oJNqBozTYBVqFHLzZ_W9vM",
+  authDomain: "fs--clone.firebaseapp.com",
+  projectId: "fs--clone",
+  storageBucket: "fs--clone.appspot.com",
+  messagingSenderId: "223609104288",
+  appId: "1:223609104288:web:9d878ad56b67cc6bcce6c5"
+};
