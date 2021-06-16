c503938ef7a79111cee1cfe54347b45c534af1fa
Sudhanshu <bsudhanshu3@gmail.com>
2021-06-17 01:26:03 +0530

    made initial header UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Sudhanshu</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
