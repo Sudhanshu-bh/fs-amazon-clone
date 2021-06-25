e4eec865424cdee058ee07429f3f0439dd22b014
Sudhanshu <bsudhanshu3@gmail.com>
2021-06-25 16:07:54 +0530

    corrected issues leading to firebase functions deploy error

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")('sk_test_51J5T7DSIJRW4yvGoOXHUf31ORqHOAzqhQra8HkvDTcIUqeOvb6LrUYjjXV6tuW1D1Kg4e0Op5bxaB9yR51PwhrlY00ApdNsGxR')
+const stripe = require("stripe")("sk_test_51J5T7DSIJRW4yvGoOXHUf31ORqHOAzqhQra8HkvDTcIUqeOvb6LrUYjjXV6tuW1D1Kg4e0Op5bxaB9yR51PwhrlY00ApdNsGxR")
 
 // API
 
@@ -13,9 +13,9 @@ app.use(cors({ origin: true }))
 app.use(express.json())
 
 // - API routes
-app.get('/', (req, res) => res.status(200).send('hello world'))
+app.get("/", (req, res) => res.status(200).send("hello world"))
 
-app.post('/payments/create', async (req, res) => {
+app.post("/payments/create", async (req, res) => {
   const total = req.query.total;
 
   const paymentIntent = await stripe.paymentIntents.create({
--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/fs--clone/us-central1/api'
-  // the API (cloud function) URL
+  baseURL: "https://us-central1-fs--clone.cloudfunctions.net/api"
+  // 'http://localhost:5001/fs--clone/us-central1/api'
 });
 
 export default instance;
